291d00cfec58562dbcac3cac9ff9f7bb82b6b993
xieyijie <xieyijie98@gmail.com>
2021-11-11 20:46:48 +0800

    Code structure improvement

--- src/main/java/hk/hku/dashboard/controller/DashboardController.java
+++ src/main/java/hk/hku/dashboard/controller/DashboardController.java
@@ -1,13 +1,22 @@
 package hk.hku.dashboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("dashboard/production")
-    public String routeProduction() {
+    public String routeProduction(Model model) {
+        Map<String, Object> student = new HashMap<>();
+        student.put("name", "David");
+        student.put("age", 18);
+        model.addAttribute("student", student);
+
         return "dashboard/production";
     }
 
