5df915fdfac2faa9beb2bcdd5f511576d3020542
xieyijie <xieyijie98@gmail.com>
2021-11-18 16:58:00 +0800

    Changed the logic for the expansion or collapse of the left navbar

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -2,7 +2,7 @@ $(function () {
     // recover the left container status when the page is ready
     // the status is stored in the session storage
     if (window.sessionStorage.getItem('left-container-status') === 'expanded') {
-        $('#navbar-left-toggle').click();
+        expandLeftContainer();
     }
 
     // set the link corresponding to current page active
@@ -32,28 +32,33 @@ $(window).on('load', function () {
 $('#navbar-left-toggle').click(function () {
     if (!$('#container-left').hasClass('expanded')) {
         // click to expand when the left container is collapsed
-        $('#container-left').addClass('expanded');
-        $('#navbar-left-toggle i').removeClass('fa-angle-double-right').addClass('fa-angle-double-left');
-        $('#container-right').css('margin-left', '280px');
-        // store the left container status in session storage
-        window.sessionStorage.setItem('left-container-status', 'expanded');
+        expandLeftContainer();
     } else {
         // click to collapse when the left container is expanded
-        $('#container-left').removeClass('expanded');
-        $('#navbar-left-toggle i').removeClass('fa-angle-double-left').addClass('fa-angle-double-right');
-        $('#container-right').css('margin-left', '70px');
-        // recover the float expand button in mobile device view after the collapse
-        // remove the .expanded class anyway when the collapse button is clicked
-        $('#float-expand-btn').removeClass('expanded');
-        // remove the left container status in session storage when it is collapsed
-        window.sessionStorage.removeItem('left-container-status');
+        collapseLeftContainer();
     }
 });
 
 // expand left container in mobile device view
 $('#float-expand-btn').click(function () {
-    if (!$('#container-left').hasClass('expanded')) {
-        $('#navbar-left-toggle').click();
-    }
-    $(this).addClass('expanded');
+    expandLeftContainer();
 });
+
+function expandLeftContainer() {
+    $('#container-left').addClass('expanded');
+    $('#container-right').addClass('shrunk');
+    // store the left container status in session storage
+    window.sessionStorage.setItem('left-container-status', 'expanded');
+    // add the .hidden class anyway when the expand button is clicked
+    $('#float-expand-btn').addClass('hidden');
+}
+
+function collapseLeftContainer() {
+    $('#container-left').removeClass('expanded');
+    $('#container-right').removeClass('shrunk');
+    // remove the left container status in session storage when it is collapsed
+    window.sessionStorage.removeItem('left-container-status');
+    // recover the float expand button in mobile device view after the collapse
+    // remove the .hidden class anyway when the collapse button is clicked
+    $('#float-expand-btn').removeClass('hidden');
+}
