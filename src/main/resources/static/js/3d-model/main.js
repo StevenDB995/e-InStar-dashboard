c595894b5511ac0b358a9d7434c463567ed2b49f
stevendb <xieyijie98@gmail.com>
2022-04-29 17:27:33 +0800

    Latest version of three.js

--- src/main/resources/static/js/3d-model/main.js
+++ src/main/resources/static/js/3d-model/main.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.module.js';
+import * as THREE from '../threejs/Three.js';
 import {PLYLoader} from './PLYLoader.js';
 import {OrbitControls} from './OrbitControls.js';
 import {LogisticsMap} from '../map/LogisticsMap.js';
