e2e4fe31844e9fbc2e913f29f9b92eeda242aa94
xieyijie <xieyijie98@gmail.com>
2022-06-01 19:38:08 +0800

    Realistic data for the press release

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,4 +1,3 @@
-import {workStageData} from './work-stage-data.js';
 import * as barChartData from './monthly-stat-data.js';
 import {LogisticsMap} from './map/LogisticsMap.js';
 import BIM from './3d-model/main.js';
@@ -38,77 +37,104 @@ onHashChange();
 
 /*
 ------------------------------------------------------------------------------------------------------------------------
-get data
+dashboard data & global variables
  */
 
-let productionTotal = 952;
-let productionProgressCount = 142;
-let productionCompletedCount = 285;
-let logisticsCount = 86;
-let installationCount = 72;
+let productionTotal = 0;
+let productionProgressCount = 0;
+let productionCompletedCount = 0;
+let logisticsCount = 0;
+let installationCount = 0;
 
-let productionProgressRate = productionProgressCount / productionTotal;
-let productionCompletedRate = productionCompletedCount / productionTotal;
-let installationRate = installationCount / productionTotal;
+let productionProgressRate;
+let productionCompletedRate;
+let installationRate;
+
+const apiUrlPrefix = 'http://147.8.139.123/api';
 
 /*
 ------------------------------------------------------------------------------------------------------------------------
-header (with swiper)
+fill data
  */
 
-// fill data
-$('#production-progress-percentage').text(
-    productionProgressRate.toFixed(2)
-    * 100 + '%'
-);
+$.ajax({
+    url: `${apiUrlPrefix}/statTask/getTask`,
+    method: 'get',
+    cache: false,
+    success: function (data) {
+        productionTotal = data['total_module'];
+        productionProgressCount = data['in_progress_module'];
+        productionCompletedCount = data['completed_module'];
 
-$('#production-completed-percentage').text(
-    productionCompletedRate.toFixed(2)
-    * 100 + '%'
-);
+        productionProgressRate = productionProgressCount / productionTotal;
+        productionCompletedRate = productionCompletedCount / productionTotal;
+        installationRate = installationCount / productionTotal;
 
-$('#logistics-count').text(logisticsCount);
+        $('#production-progress-percentage').text(
+            productionProgressRate.toFixed(2)
+            * 100 + '%'
+        );
 
-$('#installation-percentage').text(
-    installationRate.toFixed(2)
-    * 100 + '%'
-);
+        $('#production-completed-percentage').text(
+            productionCompletedRate.toFixed(2)
+            * 100 + '%'
+        );
+
+        $('#production-progress-count').text(productionProgressCount);
+        $('#production-completed-count').text(productionCompletedCount);
+        $('#production-total').text(productionTotal);
+
+        // render the diagrams
+        let productionCompletedSquares = productionCompletedRate.toFixed(2) * 100;
+        let productionProgressSquares = productionProgressRate.toFixed(2) * 100;
+        let installationSquares = installationRate.toFixed(2) * 100;
+
+        // for (let i = 1; i <= productionCompletedSquares; ++i) {
+        //     $('#pp-square-' + i).addClass('pp-square-blue');
+        //     $('#pc-square-' + i).addClass('pc-square-blue');
+        // }
+        //
+        // for (let i = productionCompletedSquares + 1;
+        //      i <= productionCompletedSquares + productionProgressSquares;
+        //      ++i) {
+        //     $('#pp-square-' + i).addClass('pp-square-yellow');
+        //     $('#pc-square-' + i).addClass('pc-square-yellow');
+        // }
+
+        for (let i = 1; i <= productionProgressSquares; ++i) {
+            $('#pp-square-' + i).addClass('pp-square-yellow');
+        }
+
+        for (let i = 1; i <= productionCompletedSquares; ++i) {
+            $('#pc-square-' + i).addClass('pc-square-blue');
+        }
+
+        for (let i = 1; i <= installationSquares; ++i) {
+            $('#in-square-' + i).addClass('in-square-green');
+        }
+
+        for (let i = installationSquares + 1;
+             i <= productionCompletedSquares;
+             ++i) {
+            $('#in-square-' + i).addClass('in-square-blue');
+        }
+
+        for (let i = productionCompletedSquares + 1;
+             i <= productionCompletedSquares + productionProgressSquares;
+             ++i) {
+            $('#in-square-' + i).addClass('in-square-yellow');
+        }
+    },
+
+    error: function (res) {
+        console.error(res);
+    }
+});
 
 /*
 ------------------------------------------------------------------------------------------------------------------------
 production pages (in progress and completed)
  */
-
-let factoryStorageCount = 93;
-
-$('#production-progress-count').text(productionProgressCount);
-$('#production-completed-count').text(productionCompletedCount);
-$('#production-total').text(productionTotal);
-$('#production-factory-count').text(factoryStorageCount);
-
-// render the diagrams
-let productionCompletedSquares = productionCompletedRate.toFixed(2) * 100;
-let productionProgressSquares = productionProgressRate.toFixed(2) * 100;
-
-// for (let i = 1; i <= productionCompletedSquares; ++i) {
-//     $('#pp-square-' + i).addClass('pp-square-blue');
-//     $('#pc-square-' + i).addClass('pc-square-blue');
-// }
-//
-// for (let i = productionCompletedSquares + 1;
-//      i <= productionCompletedSquares + productionProgressSquares;
-//      ++i) {
-//     $('#pp-square-' + i).addClass('pp-square-yellow');
-//     $('#pc-square-' + i).addClass('pc-square-yellow');
-// }
-
-for (let i = 1; i <= productionProgressSquares; ++i) {
-    $('#pp-square-' + i).addClass('pp-square-yellow');
-}
-
-for (let i = 1; i <= productionCompletedSquares; ++i) {
-    $('#pc-square-' + i).addClass('pc-square-blue');
-}
 
 /*
 work stage
@@ -119,117 +145,160 @@ for (let i = 0; i < 100; ++i) {
     dots += '.';
 }
 
-workStageData.forEach(function (elem) {
-    //language=HTML
-    $('#work-stage-table').append(`
-        <div class="table-row">
-            <div class="table-cell work-stage-name">${elem.name + dots}</div>
-            <div class="table-cell work-stage-count">${elem.count} Units</div>
-        </div>
-    `);
+$.ajax({
+    url: `${apiUrlPrefix}/statTask/getUnitsinProgress`,
+    method: 'get',
+    cache: false,
+    success: function (data) {
+        $('#work-stage-table > .table-body').empty();
+        data.forEach(function (elem) {
+            //language=HTML
+            $('#work-stage-table').append(`
+                <div class="table-row">
+                    <div class="table-cell work-stage-name">${elem.name + dots}</div>
+                    <div class="table-cell work-stage-count">${elem.count} Units</div>
+                </div>
+            `);
+        });
+    },
+
+    error: function (res) {
+        const workStages = [
+            'Structure',
+            'Concreting',
+            'Door/window',
+            'Wall',
+            'Painting',
+            'MEP',
+            'Floor',
+            'Furniture',
+            'Facade',
+            'Cleaning/protection',
+            'T&C Final Check'
+        ];
+
+        workStages.forEach(function (elem) {
+            //language=HTML
+            $('#work-stage-table').append(`
+                <div class="table-row">
+                    <div class="table-cell work-stage-name">${elem + dots}</div>
+                    <div class="table-cell work-stage-count">0 Units</div>
+                </div>
+            `);
+        })
+
+        console.error(res);
+    }
 });
 
 /*
 monthly stat (bar chart)
  */
 
-for (let i = 0; i < barChartData.xAxis.length; ++i) {
-    barChartData.xAxis[i] = barChartData.xAxis[i].toUpperCase();
-}
+// emulate async
+setTimeout(function () {
+    renderBarChart(barChartData.xAxis, barChartData.yAxis);
+}, 10);
 
-const barChart = echarts.init(document.getElementById('bar-chart'));
-barChart.setOption({
-    animation: false,
-    textStyle: {
-        fontFamily: 'RobotoSlab'
-    },
-    grid: {
-        top: 40,
-        left: '12%'
-    },
-    xAxis: {
-        type: 'category',
-        data: barChartData.xAxis,
-        name: 'Month',
-        nameLocation: 'middle',
-        nameGap: 28,
-        nameTextStyle: {
-            fontSize: 14,
-            fontStyle: 'bold'
+function renderBarChart(xAxisData, yAxisData) {
+    for (let i = 0; i < xAxisData.length; ++i) {
+        xAxisData[i] = xAxisData[i].toUpperCase();
+    }
+
+    const barChart = echarts.init(document.getElementById('bar-chart'));
+    barChart.setOption({
+        animation: false,
+        textStyle: {
+            fontFamily: 'RobotoSlab'
         },
-        axisLine: {
-            symbol: ['none', 'arrow'],
-            symbolSize: [6, 9],
-            lineStyle: {
-                color: '#000'
-            }
+        grid: {
+            top: 40,
+            left: '12%'
         },
-        axisTick: {
-            show: false
-        },
-        axisLabel: {
-            fontSize: 10
-        },
-        boundaryGap: false,
-        min: function (value) {
-            return value.min - 1;
-        },
-        max: function (value) {
-            return value.max + 1;
-        }
-    },
-    yAxis: {
-        type: 'value',
-        name: 'No. of Units',
-        nameLocation: 'middle',
-        nameGap: 6,
-        nameTextStyle: {
-            fontSize: 14,
-            fontStyle: 'bold'
-        },
-        axisLine: {
-            show: true,
-            symbol: ['none', 'arrow'],
-            symbolSize: [6, 9],
-            lineStyle: {
-                color: '#000'
-            }
-        },
-        splitLine: {
-            show: false
-        },
-        axisLabel: {
-            show: false
-        },
-        max: function (value) {
-            return value.max + 30;
-        }
-    },
-    series: [
-        {
-            type: 'bar',
-            data: barChartData.yAxis,
-            label: {
-                show: true,
-                position: 'top',
-                formatter: '{c}',
-                textStyle: {
-                    fontStyle: 'bold'
+        xAxis: {
+            type: 'category',
+            data: xAxisData,
+            name: 'Month',
+            nameLocation: 'middle',
+            nameGap: 28,
+            nameTextStyle: {
+                fontSize: 14,
+                fontStyle: 'bold'
+            },
+            axisLine: {
+                symbol: ['none', 'arrow'],
+                symbolSize: [6, 9],
+                lineStyle: {
+                    color: '#000'
                 }
             },
-            itemStyle: {
-                color: '#80C1E9'
+            axisTick: {
+                show: false
+            },
+            axisLabel: {
+                fontSize: 10
+            },
+            boundaryGap: false,
+            min: function (value) {
+                return value.min - 1;
+            },
+            max: function (value) {
+                return value.max + 1;
             }
-        }
-    ]
-});
+        },
+        yAxis: {
+            type: 'value',
+            name: 'No. of Units',
+            nameLocation: 'middle',
+            nameGap: 6,
+            nameTextStyle: {
+                fontSize: 14,
+                fontStyle: 'bold'
+            },
+            axisLine: {
+                show: true,
+                symbol: ['none', 'arrow'],
+                symbolSize: [6, 9],
+                lineStyle: {
+                    color: '#000'
+                }
+            },
+            splitLine: {
+                show: false
+            },
+            axisLabel: {
+                show: false
+            },
+            max: function (value) {
+                return value.max + 30;
+            }
+        },
+        series: [
+            {
+                type: 'bar',
+                data: yAxisData,
+                label: {
+                    show: true,
+                    position: 'top',
+                    formatter: '{c}',
+                    textStyle: {
+                        fontStyle: 'bold'
+                    }
+                },
+                itemStyle: {
+                    color: '#80C1E9'
+                }
+            }
+        ]
+    });
 
-// resize when the container's width changes;
-// rerender the chart anyway when the page first loaded
-// to prevent the failure to render the chart
-new ResizeObserver(function () {
-    barChart.resize();
-}).observe(document.getElementById('bar-chart'));
+    // resize when the container's width changes;
+    // rerender the chart anyway when the page first loaded
+    // to prevent the failure to render the chart
+    new ResizeObserver(function () {
+        barChart.resize();
+    }).observe(document.getElementById('bar-chart'));
+}
 
 /*
 ------------------------------------------------------------------------------------------------------------------------
@@ -339,28 +408,6 @@ function resetMap() {
 ------------------------------------------------------------------------------------------------------------------------
 installation page
  */
-
-// fill data
-$('#installation-count').text(installationCount);
-
-// render the diagram
-let installationSquares = installationRate.toFixed(2) * 100;
-
-for (let i = 1; i <= installationSquares; ++i) {
-    $('#in-square-' + i).addClass('in-square-green');
-}
-
-for (let i = installationSquares + 1;
-     i <= productionCompletedSquares;
-     ++i) {
-    $('#in-square-' + i).addClass('in-square-blue');
-}
-
-for (let i = productionCompletedSquares + 1;
-     i <= productionCompletedSquares + productionProgressSquares;
-     ++i) {
-    $('#in-square-' + i).addClass('in-square-yellow');
-}
 
 /*
 3d model
--- src/main/resources/static/js/monthly-stat-data.js
+++ src/main/resources/static/js/monthly-stat-data.js
@@ -1,4 +1,28 @@
-const xAxis = ['Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May'];
-const yAxis = [140, 168, 158, 168, 158, 140, 178];
+const xAxis = ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+const yAxis = [0, 0, 0, 0, 1, 1, 0];
 
-export {xAxis, yAxis};
+const defaultXAxis = [];
+const defaultYAxis = [0, 0, 0, 0, 0, 0, 0];
+
+const monthMap = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec'
+};
+
+let currentMonth = new Date().getMonth();
+let startMonth = currentMonth - 6;
+for (let i = startMonth; i <= currentMonth; ++i) {
+    defaultXAxis.push(monthMap[(i < 0) ? (i + 12) : i]);
+}
+
+export {xAxis, yAxis, defaultXAxis, defaultYAxis};
